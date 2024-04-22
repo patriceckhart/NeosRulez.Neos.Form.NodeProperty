8a63d723a62395f60b3849b79473c1602c3f0223
Patric Eckhart <mail@patriceckhart.com>
2024-04-22 14:21:23 +0200

    problems with non-string datatypes fixed

---
+++ Classes/ViewHelpers/TypeToStringViewHelper.php
@@ -0,0 +1,59 @@
+<?php
+namespace NeosRulez\Neos\Form\NodeProperty\ViewHelpers;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\FluidAdaptor\Core\ViewHelper\AbstractViewHelper;
+
+class TypeToStringViewHelper extends AbstractViewHelper
+{
+
+    public function initializeArguments()
+    {
+        $this->registerArgument('propertyValue', 'mixed', 'Value of the property', false);
+        $this->registerArgument('dateTimeFormat', 'string|null', 'Format of a datetime property', false);
+    }
+
+    /**
+     * @return string
+     */
+    public function render(): string
+    {
+        if($this->getPropertyValueType() === 'object' && $this->getPropertyValueClass() === 'DateTime') {
+            return $this->getPropertyValue()->format($this->getDateTimeFormat());
+        }
+        return $this->getPropertyValue();
+    }
+    
+    /**
+     * @return string
+     */
+    public function getPropertyValueType(): string
+    {
+        return gettype($this->arguments['propertyValue']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getPropertyValueClass(): string
+    {
+        return get_class($this->arguments['propertyValue']);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPropertyValue(): mixed
+    {
+        return $this->arguments['propertyValue'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateTimeFormat(): string
+    {
+        return $this->arguments['dateTimeFormat'] !== null ? $this->arguments['dateTimeFormat'] : 'Y-m-d H:i:s';
+    }
+
+}
